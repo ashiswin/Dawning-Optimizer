2941b792f0ab1cba49e64e21c448b896420f30ae
Isaac Ashwin <ashiswin@gmail.com>
2020-12-14 22:38:03 +0800

    Added result table and updated endpoint being reached

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,54 @@ import { Container, Grid } from 'semantic-ui-react';
 import IngredientGrid from './components/IngredientGrid';
 import { Ingredients } from './providers/IngredientProvider';
 import ResultPane from './components/ResultPane';
+import axios from 'axios';
 
-interface Quantity {
+export interface Quantity {
   [index: string]: string,
+}
+
+export interface CookieResult {
+  [index: string]: {
+    total: number,
+    items: Quantity[],
+  }
 }
 
 function App() {
   const [quantities, setQuantities] = useState<Quantity>({});
+  const [cookieResult, setCookieResult] = useState<CookieResult>();
 
   const onQuantityChangeHandler = (name: string, quantity: string) => {
     let newQuantity: {[index: string]: string} = {};
     newQuantity[name] = quantity;
     setQuantities({...quantities, ...newQuantity});
+  }
+
+  const onBakeItClickHandler = () => {
+    axios.post('https://dawning-optimizer.herokuapp.com/calculate', quantities)
+    .then(function (response) {
+      const items = response.data.items.map((item: [string, number]) => {
+        let quantity:Quantity = {}; 
+        quantity[item[0]] = item[1].toString(); 
+        return quantity;
+      });
+      const mwitems = response.data.mwitems.map((item: [string, number]) => {
+        let quantity:Quantity = {}; 
+        quantity[item[0]] = item[1].toString(); 
+        return quantity;
+      });
+      console.log(items);
+      setCookieResult({
+        "nomasterwork": {
+          total: response.data.total,
+          items: items,
+        },
+        "masterwork": {
+          total: response.data.mwtotal,
+          items: mwitems,
+        }
+      })
+    })
   }
 
   return (
@@ -35,7 +71,7 @@ function App() {
           <IngredientGrid title="Kill-style Ingredients" ingredients={Ingredients.killstyle} onChange={onQuantityChangeHandler} topLevel />
         </Grid.Column>
         <Grid.Column>
-          <ResultPane text={JSON.stringify(quantities)} />
+          <ResultPane cookieResult={cookieResult} onBakeItClick={onBakeItClickHandler} />
         </Grid.Column>
       </Grid>
     </>
--- src/components/ResultPane.tsx
+++ src/components/ResultPane.tsx
@@ -1,29 +1,96 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Container, Grid, Header, Segment } from 'semantic-ui-react';
+import { Button, Container, Grid, Header, Segment, Tab, Table } from 'semantic-ui-react';
+import { CookieResult, Quantity } from '../App';
 
 interface Props {
-  text: string,
+  cookieResult?: CookieResult,
+  onBakeItClick: () => void,
 }
 
-const ResultPane: React.FC<Props> = ({text}) => {
-  const [output, setOutput] = useState(text);
-  useEffect(() => { setOutput(text) }, [text]);
+const ResultPane: React.FC<Props> = ({cookieResult, onBakeItClick}) => {
+  const [result, setResult] = useState(cookieResult);
+  const [loading, setLoading] = useState(false);
+
+  const onBakeItClickHandler = () => {
+    setLoading(true);
+    onBakeItClick();
+  }
+
+  useEffect(() => { 
+    setResult(cookieResult); 
+    setLoading(false); 
+  }, [cookieResult]);
+
+  const getResultTable = (quantities: Quantity[]) => {
+    const rows = quantities.map((quantity) => {
+      const name = Object.keys(quantity)[0];
+      return (
+        <Table.Row>
+          <Table.Cell>{name}</Table.Cell>
+          <Table.Cell>{name}</Table.Cell>
+          <Table.Cell>{quantity[name]}</Table.Cell>
+        </Table.Row>
+      );
+    });
+
+    return (
+      <Table celled style={{borderRadius: 0, backgroundColor: "#545454", color: "white"}}>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell style={styles.tableHeader}>Picture</Table.HeaderCell>
+            <Table.HeaderCell style={styles.tableHeader}>Name</Table.HeaderCell>
+            <Table.HeaderCell style={styles.tableHeader}>Quantity</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {rows}
+        </Table.Body>
+      </Table>
+    );
+  }
   
+  const panes = [
+    {
+      menuItem: `Non-Masterworked Oven (${result?.nomasterwork.total} cookies)`,
+      render: () => result !== undefined ? <Tab.Pane attached={false} style={styles.tabPane}>{getResultTable(result.nomasterwork.items)}</Tab.Pane> : null,
+    },
+    {
+      menuItem: `Masterworked Oven (${result?.masterwork.total} cookies)`,
+      render: () => result !== undefined ? <Tab.Pane attached={false} style={styles.tabPane}>{getResultTable(result.masterwork.items)}</Tab.Pane> : null,
+    },
+  ]
+
   return (
     <Container fluid style={{marginBottom: 16}}>
-      <Grid columns="two">
+      <Grid columns="two" verticalAlign="middle">
         <Grid.Column>
           <Header>Result</Header>
         </Grid.Column>
         <Grid.Column floated="right">
-          <Button content='Bake It!' primary floated="right" />
+          <Button content='Bake It!' primary floated="right" loading={loading} onClick={onBakeItClickHandler} />
         </Grid.Column>
       </Grid>
       <Segment style={{backgroundColor: "#373737", borderRadius: 0, border: "#FFFFFFF solid 0.5px"}}>
-        {output}
+        {
+          result !== undefined
+            ? <Tab menu={{ secondary: true, pointing: true }} panes={panes} style={{color: "white !important"}}/>
+            : ""
+        }
       </Segment>
     </Container>
   );
+}
+
+const styles = {
+  tabPane: {
+    backgroundColor: "#373737", 
+    boxShadow: "none", 
+    border: "none",
+  },
+  tableHeader: {
+    backgroundColor: "#545454", 
+    color: "white",
+  }
 }
 
 export default ResultPane;
